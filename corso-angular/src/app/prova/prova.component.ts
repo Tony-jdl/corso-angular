eaed37129d366ec9a9ccf2399aa2efe08b5df0b9
Tony-jdl <>
2023-07-31 17:45:33 +0200

    Lesson 5 - Live Cycle Component

--- corso-angular/src/app/prova/prova.component.ts
+++ corso-angular/src/app/prova/prova.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-prova',
   templateUrl: './prova.component.html',
   styleUrls: ['./prova.component.css']
 })
-export class ProvaComponent {
-
+export class ProvaComponent implements OnInit, AfterContentChecked, AfterContentInit, 
+AfterViewChecked, AfterViewInit, DoCheck, OnDestroy{
+  constructor(){
+    console.log("costruttore");
+  }
+  ngOnDestroy(): void {
+    console.log("ngOnDestroy");
+  }
+  ngDoCheck(): void {
+    console.log("ngDoCheck");
+  }
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit");
+  }
+  ngAfterViewChecked(): void {
+    console.log("ngAfterViewChecked");
+  }
+  ngAfterContentInit(): void {
+    console.log("ngAfterContentInit");
+  }
+  ngAfterContentChecked(): void {
+    console.log("ngAfterContentChecked");
+  }
+  ngOnInit(){
+    console.log("ngOnInit")
+  }
 }
