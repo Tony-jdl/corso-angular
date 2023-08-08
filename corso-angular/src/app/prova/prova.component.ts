603448423d811b1c5f26e4e274bc43e20f31ca34
Tony-jdl <>
2023-08-08 14:15:38 +0200

    Lesson 8 - Property Binding

--- corso-angular/src/app/prova/prova.component.ts
+++ corso-angular/src/app/prova/prova.component.ts
@@ -1,52 +1,26 @@
-import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-prova',
   templateUrl: './prova.component.html',
   styleUrls: ['./prova.component.css']
 })
-export class ProvaComponent implements OnInit, AfterContentChecked, AfterContentInit, 
-AfterViewChecked, AfterViewInit, DoCheck, OnDestroy{
+export class ProvaComponent implements OnInit{
 
-  cani =[
-    {
-      nome: "cane 1",
-      razza: "razza 1",
-      descrizione: `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
-      from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
-      originally bred for hunting.`
-    },
-    {
-      nome: "cane 2",
-      razza: "razza 2",
-      descrizione: `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
-      from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
-      originally bred for hunting.`
-    }
-  ] 
+  isDisabled = true;
+  immagine = "";
+  immagine1 = "https://material.angular.io/assets/img/examples/shiba2.jpg";
+  immagine2 = "";
 
   constructor(){
     console.log("costruttore");
   }
-  ngOnDestroy(): void {
-    console.log("ngOnDestroy");
-  }
-  ngDoCheck(): void {
-    console.log("ngDoCheck");
-  }
-  ngAfterViewInit(): void {
-    console.log("ngAfterViewInit");
-  }
-  ngAfterViewChecked(): void {
-    console.log("ngAfterViewChecked");
-  }
-  ngAfterContentInit(): void {
-    console.log("ngAfterContentInit");
-  }
-  ngAfterContentChecked(): void {
-    console.log("ngAfterContentChecked");
-  }
   ngOnInit(){
     console.log("ngOnInit")
+    let counter = 0;
+    setInterval(()=>{
+      (counter%2 == 0) ? (this.immagine = this.immagine1) : (this.immagine = this.immagine2);
+      counter++;
+    }, 1000)
   }
 }
